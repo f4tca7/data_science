87b15a32aa0564dfbaebd8d2c08e4b05105d5ab1
Toni Schmidt <toni_schmidt@ctrip.com>
2019-03-12 13:27:18 +0800

    remove comments

--- plotting/timeseries.py
+++ plotting/timeseries.py
@@ -12,59 +12,59 @@ msft = stocks[['MSFT']]
 
 # ------------ simple lineplot ------------ #
 
-# plt.plot(aapl, color='blue', label='AAPL')
-# plt.plot(ibm, color='green', label='IBM')
-# plt.plot(csco, color='red', label='CSCO')
-# plt.plot(msft, color='magenta', label='MSFT')
-# # Add a legend in the top left corner of the plot
-# plt.legend(loc='upper left')
-# # Specify the orientation of the xticks
-# plt.xticks(rotation=60)
-# plt.show()
+plt.plot(aapl, color='blue', label='AAPL')
+plt.plot(ibm, color='green', label='IBM')
+plt.plot(csco, color='red', label='CSCO')
+plt.plot(msft, color='magenta', label='MSFT')
+# Add a legend in the top left corner of the plot
+plt.legend(loc='upper left')
+# Specify the orientation of the xticks
+plt.xticks(rotation=60)
+plt.show()
 
-# # ------------ multiple time series slices ------------ #
+# ------------ multiple time series slices ------------ #
 
-# # Plot the series in the top subplot in blue
-# plt.subplot(2,1,1)
-# plt.xticks(rotation=45)
-# plt.title('AAPL: 2001 to 2011')
-# plt.plot(aapl, color='blue')
-# view = aapl['2007':'2008']
-# plt.subplot(2,1,2)
-# plt.xticks(rotation=45)
-# plt.title('AAPL: 2007 to 2008')
-# plt.plot(view, color='black')
-# plt.tight_layout()
-# plt.show()
+# Plot the series in the top subplot in blue
+plt.subplot(2,1,1)
+plt.xticks(rotation=45)
+plt.title('AAPL: 2001 to 2011')
+plt.plot(aapl, color='blue')
+view = aapl['2007':'2008']
+plt.subplot(2,1,2)
+plt.xticks(rotation=45)
+plt.title('AAPL: 2007 to 2008')
+plt.plot(view, color='black')
+plt.tight_layout()
+plt.show()
 
-# # ------------ multiple time series slices ------------ #
+# ------------ multiple time series slices ------------ #
 
-# # Slice aapl from Nov. 2007 to Apr. 2008 inclusive: view
-# view = aapl['2007-11':'2008-04']
-# plt.subplot(2,1,1)
-# plt.plot(view, color='red')
-# plt.title('AAPL: Nov. 2007 to Apr. 2008')
-# plt.xticks(rotation=45)
-# view = aapl['2008-01']
-# plt.subplot(2,1,2)
-# plt.plot(view, color='green')
-# plt.title('AAPL: Jan. 2008')
-# plt.xticks(rotation=45)
-# plt.tight_layout()
-# plt.show()
+# Slice aapl from Nov. 2007 to Apr. 2008 inclusive: view
+view = aapl['2007-11':'2008-04']
+plt.subplot(2,1,1)
+plt.plot(view, color='red')
+plt.title('AAPL: Nov. 2007 to Apr. 2008')
+plt.xticks(rotation=45)
+view = aapl['2008-01']
+plt.subplot(2,1,2)
+plt.plot(view, color='green')
+plt.title('AAPL: Jan. 2008')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 
-# # ------------ inset view ------------ #
+# ------------ inset view ------------ #
 
-# # Slice aapl from Nov. 2007 to Apr. 2008 inclusive: view
-# view = aapl['2007-11':'2008-04']
-# plt.plot(aapl)
-# plt.xticks(rotation=45)
-# plt.title('AAPL: 2001-2011')
-# plt.axes ((0.25, 0.5, 0.35, 0.35))
-# plt.plot(view, color='red')
-# plt.xticks(rotation=45)
-# plt.title('2007/11-2008/04')
-# plt.show()
+# Slice aapl from Nov. 2007 to Apr. 2008 inclusive: view
+view = aapl['2007-11':'2008-04']
+plt.plot(aapl)
+plt.xticks(rotation=45)
+plt.title('AAPL: 2001-2011')
+plt.axes ((0.25, 0.5, 0.35, 0.35))
+plt.plot(view, color='red')
+plt.xticks(rotation=45)
+plt.title('2007/11-2008/04')
+plt.show()
 
 # ------------ moving averages ------------ #
 
