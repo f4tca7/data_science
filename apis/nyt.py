e37bcf85e3bfb63cb23c56648d3655d319de9cfb
Toni Schmidt <toni.schmidt21@gmail.com>
2019-03-17 20:31:09 +0800

    added NYT API requester

---
+++ apis/nyt.py
@@ -0,0 +1,91 @@
+import requests
+import json
+import secrets
+import datetime
+import numpy as np
+import pandas as pd
+
+
+COLUMNS = ['date', 'headline', 'snippet', 'keywords']
+def json_to_df(data):   
+    """
+    Reads pub_date, headline, snippet and keywords from an NYT Archive API json document and converts them into a 2d list
+    """
+    #data = json.load(f)
+    rows = []
+    for doc in data['response']['docs']:
+        cols = []
+        cols.append(pd.to_datetime(doc['pub_date']).date())
+        cols.append(doc['headline']['main'])
+        cols.append(doc['snippet'])
+        pub_keywords = ''
+        for keyword in doc['keywords']:
+            pub_keywords =  pub_keywords + (keyword['value']) + ' '
+        cols.append(pub_keywords)
+        rows.append(cols)    
+    return rows
+
+
+def load_from__archives_api(from_year, to_year, from_month, to_month):
+    """
+    Loads data from New York Times Archives API for the specified period.
+    Returns one consolidated pandas dataframe with date index
+    Only extracts pub_date, headline, snippet and keywords
+    """
+    now = datetime.datetime.now()
+
+    if from_year > to_year :
+        raise Exception('from_year should not be larger than to_year')
+    if (from_year == to_year) & (from_month > to_month):
+        raise Exception('from_month should not be larger than to_month for the same year')   
+    if from_year < 1851 :
+        raise Exception('can olny read data from 1851 onwards')   
+    if (from_year > now.year) | (to_year > now.year) :
+        raise Exception('cannot read articles from the future')
+    if (to_year == now.year) & (to_month > now.month) :
+        raise Exception('cannot read articles from the future. Set to_month lower')
+    
+    all_rows = []
+    num_years = to_year - from_year
+    num_month = num_years * 12
+    num_month += to_month + 1
+    num_month -= from_month
+
+    curr_year = from_year
+    curr_month = from_month
+
+    for month in range(num_month) :
+        print('Fetching data for ' + str(curr_year) +'_' + str(curr_month))
+        url = 'https://api.nytimes.com/svc/archive/v1/' + str(curr_year) +'/' + str(curr_month) + '.json?api-key=' + secrets.nyt_api_key
+        resp = requests.get(url)
+        if resp.status_code != 200:
+            # This means something went wrong.
+            raise Exception('GET /archive/v1/' +  str(curr_year) +'/' + str(curr_month) + '.json?api-key=' + secrets.nyt_api_key + ' {}'.format(resp.status_code))
+        else :
+            print('200 GET ' + url)
+            json_parsed = resp.json()
+            rows = json_to_df(json_parsed)
+            if len(all_rows) == 0 :
+                all_rows = np.array(rows)
+            else :
+                all_rows = np.vstack((all_rows,rows))
+
+        if curr_month + 1 % 12 > 0 :
+            curr_month = 1
+            curr_year += 1
+        else :
+            curr_month +=1
+
+    return all_rows
+
+from_y = 2018
+to_y = 2019
+from_m = 12
+to_m = 1
+
+rows = load_from__archives_api(from_y, to_y, from_m, to_m)
+df = pd.DataFrame.from_records(rows, columns=COLUMNS, index=['date']) 
+print(df.head())
+print(df.tail())
+filename = 'nyt_archive_' + str(from_y) + '_' + str(from_m)  + '_' + str(to_y) + '_' + str(to_m) + '.csv'
+df.to_csv('../datasets/large_data/' + filename)
--- ml/deep_learning/img_recognition_keras.py
+++ ml/deep_learning/img_recognition_keras.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import Imputer
@@ -7,44 +6,100 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report 
 import keras
-from keras.layers import Dense
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from keras.models import Sequential
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import classification_report, confusion_matrix
 from keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
-
-
 from sklearn import datasets
+from keras.datasets import mnist
+from keras import backend as K
 
+img_rows, img_cols = 28, 28
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+print(K.image_data_format())
+if K.image_data_format() == 'channels_first':
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+#print(X[1,:])
+#print(mnist.iloc[105,0])
+#img = np.reshape(X[105,:], (28, 28))
+
+#print(img)
+#imgplot = plt.imshow(img)
+#plt.show()
+#quit()
 # Create the model: model
+
+#digits = datasets.load_digits()
+#X = digits.data
+#y = to_categorical(digits.target)
+# print(y.shape)
+# print(X)
+#print(X[1,:])
+#print(y)
+#quit()
+
 model = Sequential()
-digits = datasets.load_digits()
-X = digits.data
-y = to_categorical(digits.target)
-print(y.shape)
-print(X.shape)
-print(type(X))
-
 # Add the first hidden layer
-model.add(Dense(50, activation='relu', input_shape = (64,)))
+	
+# model.add(Conv2D(32, kernel_size=(3, 3),
+#                  activation='relu',
+#                  input_shape=input_shape))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(num_classes, activation='softmax'))
 
-# Add the second hidden layer
+
+
+model.add(Flatten(input_shape = input_shape))
+model.add(Dense(250, activation='relu'))
+model.add(Dense(150, activation='relu'))
 model.add(Dense(50, activation='relu'))
-
-# Add the output layer
+model.add(Dense(50, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(150, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
 # Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
+early_stopping_monitor = EarlyStopping(patience=5)
 # Fit the model
-model_1_training = model.fit(X, y, validation_split=0.3, epochs=20)
-# model_1_training = model_1.fit(predictors, target, epochs=40, validation_split=0.2, callbacks=[early_stopping_monitor])
+model_1_training = model.fit(X_train, y_train, epochs=epochs, callbacks=[early_stopping_monitor], batch_size=batch_size, validation_data=(X_test, y_test))
 
 # Create the plot
 plt.plot(model_1_training.history['val_loss'], 'r')
 plt.xlabel('Epochs')
 plt.ylabel('Validation score')
 plt.show()
+
+score = model.evaluate(X_test, y_test, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
